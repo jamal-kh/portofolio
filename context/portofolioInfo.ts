a7fb8eb7e93cc2e8ce4952ee63870f3cc5c4a684
jamal-kh <jamalalbeenkhalil@gmail.com>
2025-09-20 23:43:57 +0300

    init commit

--- context/portofolioInfo.ts
+++ context/portofolioInfo.ts
@@ -5,8 +5,8 @@ import { FaGithub, FaLinkedin} from 'react-icons/fa';
 const Info: IPersonalInfo = {
     fullName: "Jamal Aldeen Khalil",
     jobs: [
-        "Backend Developer",
-        "Cybersecurity"
+        "Fullstack Developer",
+
     ],
     links: [
         {
