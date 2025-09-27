b8cc274922575047f90b54e36e132ab7ae68e4f8
jamal-kh <jamalalbeenkhalil@gmail.com>
2025-09-27 03:23:50 +0300

    edite

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default function App() {
   return (
     <div className="w-[700px] min-w-[350px] h-[400px]  shadow-2xl p-[20px] rounded-[15px] ">
       <div className="border h-[100%] p-[20px] rounded-[10px]">
-
         <div className="flex h-[100%]">
          <Home />
         </div>
